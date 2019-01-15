8fe4ab1d9cd7d3a778464c979ea9fbc412e0c963
Rohan Anand <thededlier@gmail.com>
2019-01-15 17:27:54 +0530

    map similar and popular movies

--- recomender.py
+++ recomender.py
@@ -1,6 +1,7 @@
 # Similar Movies example using movie lens data for example
 
 import pandas as pd
+import numpy as np
 
 # Load data
 rating_cols = ['user_id', 'movie_id', 'rating']
@@ -18,5 +19,15 @@ star_wars_ratings = movie_ratings['Star Wars (1977)']
 
 similar_movies =  movie_ratings.corrwith(star_wars_ratings).dropna()
 df = pd.DataFrame(similar_movies)
+print('--- Similar Movies ---')
+print(similar_movies.sort_values(ascending=False)[:10])
 
-print(similar_movies.sort_values(ascending=False))
+movie_stats = ratings.groupby('title').agg({ 'rating': [np.size, np.mean] })
+# Movies with greater than 125 ratings
+popular_movies = movie_stats['rating']['size'] >= 125
+print('--- Popular Movies ---')
+print(movie_stats[popular_movies].sort_values([('rating', 'mean')], ascending=False)[:10])
+
+df = movie_stats[popular_movies].join(pd.DataFrame(similar_movies, columns=['similarity']))
+print('--- Most popular and similar movies ---')
+print(df.sort_values(['similarity'], ascending=False)[:10])
